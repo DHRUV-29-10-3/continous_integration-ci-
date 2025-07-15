388d04200aaadd15305e981b8ca3185ae7b6b9b3
Dhruv Parikh <dhruvparikh291@gmail.com>
2025-07-15 10:37:10 +0530

    first

---
+++ app.py
@@ -0,0 +1,10 @@
+# linting issues : havig syntax errors and not proper presentation of codes
+def add(a, b):
+    return a+b 
+def sub(a,b):return a-b 
+
+
+res1 = add(5,4)
+res2 = sub(5,4)
+
+print(res1, res2) 
